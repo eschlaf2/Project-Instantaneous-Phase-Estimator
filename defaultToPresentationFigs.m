a3a39e6e615152e7d7a65926e23583d885a690a1
Emily <eds2@bu.edu>
2018-01-26 21:46:24 -0500

    Make pretty figs

---
+++ defaultToPresentationFigs.m
@@ -0,0 +1,8 @@
+function defaultToPresentationFigs()
+
+set(groot, 'defaultLineLineWidth', 2);
+set(groot, 'defaultAxesLineWidth', 2);
+set(groot, 'defaultFigureColor', [1 1 1], 'defaultAxesFontSize', 12, 'defaultHistogramLineWidth',1);
+set(groot, 'defaultAxesFontWeight', 'bold')
+
+
